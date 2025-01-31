d4603f487d873a0ea570195d0e56984c728119ba
Maria-Rodriguez01 <rodriguez_maria@unitec.edu>
2025-01-31 14:57:31 -0600

    Aporte Maria

---
+++ src/repositorio2lab2/EmpleadosVentas.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package repositorio2lab2;
+
+/**
+ *
+ * @author Maria Gabriela
+ */
+public class EmpleadosVentas {
+    
+}

de0f28b5333c25ade938baef91c115b7da76c4f4
camilaflores07 <camila_maria@unitec.edu>
2025-01-31 13:18:13 -0600

    holis

---
+++ src/repositorio2lab2/Repositorio2Lab2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package repositorio2lab2;
+
+/**
+ *
+ * @author 50494
+ */
+public class Repositorio2Lab2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

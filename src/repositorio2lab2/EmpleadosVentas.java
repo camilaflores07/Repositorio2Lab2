1b1da7e82a99872b8be4a477f9b5edcd09d3f2ca
Maria-Rodriguez01 <rodriguez_maria@unitec.edu>
2025-01-31 15:00:14 -0600

    Aporte Maria intento dos

--- src/repositorio2lab2/EmpleadosVentas.java
+++ src/repositorio2lab2/EmpleadosVentas.java
@@ -4,10 +4,48 @@
  */
 package repositorio2lab2;
 
+import java.util.Calendar;
+
 /**
  *
  * @author Maria Gabriela
  */
-public class EmpleadosVentas {
-    
+public class EmpleadosVentas extends Empleado{
+     private int[] ventasmen;
+    private double tasacomision;
+
+    public EmpleadosVentas(int CodigoUnico, String Nombre, double SalarioBase, int HorasTrabajadas, double tasaComision) {
+        super(CodigoUnico, Nombre, SalarioBase,FechaContratacion, HorasTrabajadas);
+        this.tasacomision = tasacomision;
+        this.ventasmen = new int[12];
+    }
+
+    public void RegistrarVenta(int mes, double monto) {
+        if (mes >= 0 && mes < 12) {
+            ventasmen[mes] += monto;
+        }
+    }
+
+    private double calcularComision() {
+        int mesActual = Calendar.getInstance().get(Calendar.MONTH);
+        return ventasmen[mesActual] * tasacomision;
+    }
+
+    public double calcularPago() {
+        return (SalarioBase / 160) * HorasTrabajadas + calcularComision();
+    }
+
+    public double calcularVentasAnuales() {
+        double total = 0;
+        for (double venta : ventasmen) {
+            total += venta;
+        }
+        return total;
+    }
+
+    @Override
+    public void MostrarInformacion() {
+        return super.MostrarInformacion() + ", Ventas anuales: " + calcularVentasAnuales();
+    }
+
 }

1906d5fa18e50b1d5a0bd26f11c503b2a08d1e73
camilaflores07 <camila_maria@unitec.edu>
2025-01-31 14:14:04 -0600

    Terminar las funciones

--- src/repositorio2lab2/Empleado.java
+++ src/repositorio2lab2/Empleado.java
@@ -10,11 +10,15 @@ public class Empleado {
     int CodigoUnico;
     String Nombre;
     Calendar FechaContratacion;
+    double SalarioBase;
+    int HorasTrabajadas;
 
-    public Empleado(int CodigoUnico, String Nombre, Calendar FechaContratacion) {
+    public Empleado(int CodigoUnico, String Nombre, Calendar FechaContratacion, double SalarioBase, int HorasTrabajadas) {
         this.CodigoUnico = CodigoUnico;
         this.Nombre = Nombre;
         this.FechaContratacion = Calendar.getInstance();
+        this.SalarioBase = SalarioBase;
+        this.HorasTrabajadas = 0;
     }
     
     public String FechaContratacion(){
@@ -23,6 +27,32 @@ public class Empleado {
         int anio = FechaContratacion.get(Calendar.YEAR);
         
         return String.format("%02d/%02d/%04d",dia, mes, anio);
+    }
+    
+    public void RegistroHorasTrabajadas(int horas){
+        if (horas<0) {
+            System.out.println("No existen horas negativas");
+            return;
+        }
+        this.HorasTrabajadas += horas;
+        System.out.println("El numero de horas trabajados son: "+ horas);
+    }
+    
+    public double CalculoDePago(){
+        int HorasDelMes = 160;
+        double SalarioProporcional = (SalarioBase/HorasDelMes)*HorasTrabajadas;
+        double SalarioFinal = SalarioProporcional * 0.035;
+        return SalarioFinal;
+        
+    }
+    
+    public void MostrarInformacion(){
+        System.out.println("El nombre del empleado es: " +Nombre);
+        System.out.println("El salario base del empleado "+Nombre+" es de: " + SalarioBase);
+        System.out.println("El empleado "+ Nombre+ "Trabajo "+HorasTrabajadas+" horas");
+        System.out.println("Su salario final es de: "+ CalculoDePago());
+                
+        
     }
         
     
